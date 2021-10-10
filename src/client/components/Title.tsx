147b61553343fc5814eed5e480c170f0eedbd9fd
Yuri Toledo <yuriwtoledo@gmail.com>
2021-10-10 16:15:02 -0300

    feat: create Title component

---
+++ src/client/components/Title.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+import styled from "styled-components";
+
+const Title: FC = ({ children }) => (
+  <P>{children}</P>
+)
+
+const P = styled.p``
+
+export default Title
