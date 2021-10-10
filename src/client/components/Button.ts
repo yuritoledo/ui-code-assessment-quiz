b5a961e18c601b99605a0ca5cbd0fd14566e8e45
Yuri Toledo <yuriwtoledo@gmail.com>
2021-10-10 17:23:27 -0300

    feat: styled the "Next" button

---
+++ src/client/components/Button.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+const Button = styled.button`
+  border: none;
+  background-color: #1C70DD;
+  color: white;
+  padding: 10px 20px;
+  text-transform: capitalize;
+  cursor: pointer;
+`
+
+export default Button
