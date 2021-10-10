ed37f004e8cf14076cc0a120bcc87f12cb820fc1
Yuri Toledo <yuriwtoledo@gmail.com>
2021-10-10 17:58:25 -0300

    feat: create AnswerContainer component

---
+++ src/client/components/AnswerContainer.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+const AnswerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+  margin-bottom: 30px;
+`
+
+export default AnswerContainer
