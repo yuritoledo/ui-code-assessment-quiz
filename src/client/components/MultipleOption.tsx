18ae807eb62e51cf41dffc721fa518ba0116410e
Yuri Toledo <yuriwtoledo@gmail.com>
2021-10-10 19:09:33 -0300

    style: removing unused import' ~

--- src/client/components/MultipleOption.tsx
+++ src/client/components/MultipleOption.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, FC, Fragment } from 'react'
+import React, { ChangeEvent, FC } from 'react'
 import { Question } from '../interfaces'
 import AnswerContainer from './AnswerContainer'
 import RadioInput from './RadioInput'
