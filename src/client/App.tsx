97583a6e23b1f1ac27c79429430b1b23ee2fe5f5
Yuri Toledo <yuriwtoledo@gmail.com>
2021-10-10 17:23:06 -0300

    feat: applying open sans to global app

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import styled from 'styled-components'
 import { QuizProvider } from "./hooks/useQuiz"
-import { Quiz } from "./page/Quiz"
+import Quiz from "./page/Quiz"
+
+const Container = styled.div`
+  font-family: 'Open Sans', sans-serif;
+`
 
 export const App = () => (
-  <QuizProvider>
-    <Quiz />
-  </QuizProvider>
+  <Container>
+    <QuizProvider>
+      <Quiz />
+    </QuizProvider>
+  </Container>
 )
